5c0c76278b03e33a260b142a2228ee8a2ea8d57e
Ankaleonova <ankaleonova@gmail.com>
2026-02-27 11:50:39 +0100

    add city indez

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 4;
+const cityIndex = city.indexOf('Y');
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = 'York';
+const citySubstring = '';
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = 'New Delhi';
+const cityReplaced = '';
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = `${city} ${cityName}`;
+const cityNameConcat = '';
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = hello.toUpperCase();
+const upperCaseHello = '';
 console.log(upperCaseHello); // Output: "HELLO"
